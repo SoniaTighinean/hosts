198e0b74a54fd88b0798e8d2e0ab0f9d03e3796c
Sonia <tighinean.sonia@yahoo.com>
2024-11-22 18:16:00 +0000

    Aduagat scriptul pentru validarea adreselor IP din /etc/hosts

---
+++ script1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+host_name=$1
+ip_address=$2
+dns_server=$3
+
+ip_correct=$(dig +short "$host_name"  @"$dns_server" | tail -n 1)
+
+if  [[ "$ip_address" == "$ip_correct" ]]; then
+	echo "OK"
+else
+	echo "NU"
+fi
+
+exit 0
